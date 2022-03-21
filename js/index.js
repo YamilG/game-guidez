efb269e3802129b21f077504dcf4fa06fcbc592a
Yamil Gonzales <yamilg@gmail.com>
2022-03-20 21:10:06 -0600

    added bio to user profile

--- js/index.js
+++ js/index.js
@@ -12,26 +12,6 @@ const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
 
-const setupUI = user => {
-  if (user) {
-    // display account info
-    const html = `<div>Logged in as ${user.email}</div>`;
-    accountDetails.innerHTML = html;
-
-    // display links
-    loggedInLinks.forEach(link => (link.style.display = 'block'));
-    loggedOutLinks.forEach(link => (link.style.display = 'none'));
-  } else {
-    //remove account info
-    const html = '';
-    accountDetails.innerHTML = html;
-
-    //remove links
-    loggedInLinks.forEach(link => (link.style.display = 'none'));
-    loggedOutLinks.forEach(link => (link.style.display = 'block'));
-  }
-};
-
 // setup guides
 const guideList = document.querySelector('.guides');
 
